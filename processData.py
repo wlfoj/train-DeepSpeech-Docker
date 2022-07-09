b3fb20f630243ed39ab18a211747eade49c4714b
Washington Oliveira <wolivej@gmail.com>
2022-07-08 23:39:35 -0300

    add encoding

--- processData.py
+++ processData.py
@@ -11,7 +11,7 @@ class ProcessPhrases():
         arquivos = [pathTest, pathTrain, pathDev]
         for i in range(3):
             # Entra em cada arquivo
-            with open(arquivos[i]) as csv_file:
+            with open(arquivos[i], 'r', encoding = 'utf-8') as csv_file:
                 csv_reader = csv.reader(csv_file, delimiter=',')
                 texto = []
                 # Passando por cada linha
@@ -32,12 +32,12 @@ class ProcessPhrases():
                         linha[2] = linha[2].replace("”", "")
                     if("«" in linha[2]):
                         linha[2] = linha[2].replace("«", "")
-                    if("è" in linha[2]):
-                        linha[2] = linha[2].replace("è", "é")
+                    if("ñ" in linha[2]):
+                        linha[2] = linha[2].replace("ñ", "n")
                     # Adicionando o novo conteudo na lista
                     texto.append([linha[0],linha[1],linha[2]])
             # Atualizando os arquivos
-            with open(arquivos[i], 'w') as arquivo_csv:
+            with open(arquivos[i], 'w', encoding = 'utf-8') as arquivo_csv:
                 escrever = csv.writer(arquivo_csv, delimiter=',', lineterminator='\n')
                 for j in range(len(texto)):
                     escrever.writerow(texto[j])
@@ -45,12 +45,12 @@ class ProcessPhrases():
 
     @classmethod
     def cleanTotalText(self, pathTest: str, pathTrain: str, pathDev: str):
-        '''Método para realizar uma limpeza completa nos carcteres lixos'''
+        '''Método para realizar uma limpeza completa nos carcteres lixos e remove todos os acentos'''
         # 0-> path test    1-> path train   2-> path dev
         arquivos = [pathTest, pathTrain, pathDev]
         for i in range(3):
             # Entra em cada arquivo
-            with open(arquivos[i]) as csv_file:
+            with open(arquivos[i], 'r', encoding = 'utf-8') as csv_file:
                 csv_reader = csv.reader(csv_file, delimiter=',')
                 texto = []
                 # Passando por cada linha
@@ -100,7 +100,7 @@ class ProcessPhrases():
                     # Adicionando o novo conteudo na lista
                     texto.append([linha[0],linha[1],linha[2]])
             # Atualizando os arquivos
-            with open(arquivos[i], 'w') as arquivo_csv:
+            with open(arquivos[i], 'w', encoding = 'utf-8') as arquivo_csv:
                 escrever = csv.writer(arquivo_csv, delimiter=',', lineterminator='\n')
                 for j in range(len(texto)):
                     escrever.writerow(texto[j])
@@ -108,19 +108,21 @@ class ProcessPhrases():
 
 def main():
     '''Procedimento para chamar e tratar os possíveis erros do processamento das falas'''
-
+    arg1 = sys.argv[1].replace('--','')
+    arg2 = sys.argv[2].replace('--','')
+    arg3 = sys.argv[3].replace('--','')
     try:
-        ProcessPhrases.cleanText(sys.argv[1], sys.argv[2], sys.argv[3])
+        ProcessPhrases.cleanTotalText(arg1, arg2, arg3)
     except IndexError:
         print("ERRO\nA chamada do arquivo deve ter 3 parametros")
         print("1 -> o caminho para a tabela de teste")
         print("2 -> o caminho para a tabela de treinamento")
         print("3 -> o caminho para a tabela de desenvolvimento")
     except FileNotFoundError:
-        print("Arquivo não encontrado")
-        print("Algum dos caminhos está incorreto")
-    except:
-        print("Error\nNão sei qual é")
+        print("Arquivo não encontrado\nOs caminhos estão incorretos")
+    except UnicodeEncodeError:
+        print("Erro de encode\nTente um formato diferente do utf-8")
+    # Se der outro erro, vai aparecer aqui
 
 
 if __name__ == "__main__":
